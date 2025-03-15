c8749818c8cac932124df03625064803e17382de
Whotfisgilang <gemilang.lingua@gmail.com>
2025-03-15 22:07:38 +0700

    Update login.sh

--- soal_2/login.sh
+++ soal_2/login.sh
@@ -14,13 +14,14 @@ echo "Enter password:"
 read -s login_pass
 echo ""
 
-# if condition check di data/player.csv
-if grep -q ".*, ${login_email}, ${login_pass}" "data/player.csv"
+# Hash input password dengan static salt
+salt="arcaea_secure_salt"
+hashed_input=$(echo -n "$salt$login_pass" | sha256sum | awk '{print $1}')
+
+# Cek apakah email dan hash cocok di database
+if grep -q "^.*, $login_email, $hashed_input" "data/player.csv"
 then
   echo "Login successful!"
 else
   echo "Login failed..."
 fi
-
-
-
