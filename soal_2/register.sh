7f56707caae271062e65c6b781a49f19f2ad7546
Whotfisgilang <gemilang.lingua@gmail.com>
2025-03-15 22:06:48 +0700

    Update register.sh

--- soal_2/register.sh
+++ soal_2/register.sh
@@ -3,19 +3,25 @@
 # Input Username
 echo "Enter username:"
 read uname
-
+  
 while [ 1 ] # Cek email input (Radiant Genesis / B)
 do
   # Input Email
   echo "Enter email:"
   read email
-  if [[ ! "$email" =~ @.*\. ]] # cek email apakah memiliki pola @<terserah>. 
+  if [[ ! "$email" =~ @.*\. ]] # cek email apakah memiliki pola @<terserah>.
   then
     echo "Invalid email!"
   else
     break # keluar dari loop
-  fi
+  fi  
 done
+  
+# Cek apakah email sudah ada
+if grep -q ".*, $email, .*" data/player.csv; then
+  echo "Email sudah digunakan! Registrasi gagal."
+  exit 1
+fi
 
 while  [ 1 ] # Cek password input (Radiant Genesis / B)
 do
@@ -37,16 +43,19 @@ do
     break # keluar dari loop
   fi
 done
-
+# Hash password dengan static salt
+salt="arcaea_secure_salt"
+hashed_pass=$(echo -n "$salt$pass" | sha256sum | awk '{print $1}')
+  
 echo ""
-
+    
 # Cek udh ada belum filenya
 if  [[ ! -f data/player.csv ]]
 then
   mkdir -p data
   echo "username, email, password" > data/player.csv
-fi
-
+fi  
+  
 #kalau udh ada ya append aja
-echo "${uname}, ${email}, ${pass}" >> data/player.csv
+echo "${uname}, ${email}, ${hashed_pass}" >> data/player.csv
 echo -e "Data saved successfully..."
